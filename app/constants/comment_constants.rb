43d8c3a5441df7f1a6946a9a6405a27ed03144d7
K Sashi Kumar <ksashikumark93@gmail.com>
2017-07-15 13:14:22 +0530

    Update comment_constants.rb

--- app/constants/comment_constants.rb
+++ app/constants/comment_constants.rb
@@ -1,5 +1,5 @@
 module CommentConstants
+  COMMENTABLE_HASH_FIELDS = %w(id type).freeze
   UPDATE_FIELDS = [:content, :parent_id, :user_id, { commentable: COMMENTABLE_HASH_FIELDS }].freeze
   UPDATE_FIELDS = [:content, :parent_id, :user_id].freeze
-  COMMENTABLE_HASH_FIELDS = %w(id type).freeze
 end.freeze
